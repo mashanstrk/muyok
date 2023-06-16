8a1b3fad66bf48e95a95843f8bcabd6cd1b8bd10
mashanstrk <117375259+mashanstrk@users.noreply.github.com>
2023-06-17 00:29:17 +0300

    mobile menu fixed

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,22 +1,38 @@
-(() => {
-    const mobileMenu = document.querySelector('.js-menu-container');
-    const openMenuBtn = document.querySelector('.js-open-menu');
-    const closeMenuBtn = document.querySelector('.js-close-menu');
+// (() => {
+//     const mobileMenu = document.querySelector('.js-menu-container');
+//     const openMenuBtn = document.querySelector('.js-open-menu');
+//     const closeMenuBtn = document.querySelector('.js-close-menu');
   
-    const toggleMenu = () => {
-      const isMenuOpen =
-        openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
-      openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
-      mobileMenu.classList.toggle('is-open');
-    };
+//     const toggleMenu = () => {
+//       const isMenuOpen =
+//         openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//       openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//       mobileMenu.classList.toggle('is-open');
+//     };
   
-    openMenuBtn.addEventListener('click', toggleMenu);
-    closeMenuBtn.addEventListener('click', toggleMenu);
+//     openMenuBtn.addEventListener('click', toggleMenu);
+//     closeMenuBtn.addEventListener('click', toggleMenu);
   
-    // Close the mobile menu on wider screens if the device orientation changes
-    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
-      if (!e.matches) return;
-      mobileMenu.classList.remove('is-open');
-      openMenuBtn.setAttribute('aria-expanded', false);
-    });
-  })();
+//     // Close the mobile menu on wider screens if the device orientation changes
+//     window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+//       if (!e.matches) return;
+//       mobileMenu.classList.remove('is-open');
+//       openMenuBtn.setAttribute('aria-expanded', false);
+//     });
+//   })();
+
+const mobileMenu = document.querySelector('.js-menu-container');
+const openMenuBtn = document.querySelector('.js-open-menu');
+const menuIcon = document.querySelector('.js-menu-icon');
+
+openMenuBtn.addEventListener("click", function() {
+  if (mobileMenu.classList.contains("closed")) {
+    mobileMenu.classList.remove("closed");
+    mobileMenu.classList.add("is-open");
+    menuIcon.setAttribute("href", "/symbol-defs.a8b2e413.svg#icon-close");
+  } else {
+    mobileMenu.classList.remove("is-open");
+    mobileMenu.classList.add("closed");
+    menuIcon.setAttribute("href", "/symbol-defs.a8b2e413.svg#icon-menu");
+  }
+});
